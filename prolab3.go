99ef9b0e422310fb62c4c563b3e21d6963894253
Ömrüm Baki Temiz <omrumbakitemiz@icloud.com>
2017-08-07 18:57:27 +0300

    haftanın günleri map[string]string olarak eklendi ve çıktının Türkçe'ye çevirildi

--- prolab3.go
+++ prolab3.go
@@ -6,23 +6,32 @@ import (
 )
 
 func main() {
-	p := fmt.Println //ekrana yazdırma fonksiyonunu bir değişkene ata
-	s := fmt.Scanln	//klavyeden veri okuma fonksiyonunu bir değişkene ata
+	for {
+		p := fmt.Println //ekrana yazdırma fonksiyonunu bir değişkene ata
+		s := fmt.Scanln	//klavyeden veri okuma fonksiyonunu bir değişkene ata
+		m := map[string]string{
+			"Monday": "Pazartesi", "Tuesday": "Salı",
+			"Wednesday": "Çarşamba", "Thursday": "Perşembe",
+			"Friday": "Cuma", "Saturday": "Cumartesi", "Sunday": "Pazar"}
 
-	var input string
-	layout := "02-01-2006" // tarih şablonu
+		var input string
+		layout := "02-1-2006" // tarih şablonu
 
-	p("Tarih girin: ")
-	s(&input)
-	p("Girdiğiniz tarih: " + input)
+		p("Tarih girin: ")
+		s(&input)
 
-	str := input
+		str := input
 
-	t, err := time.Parse(layout, str) //string olarak girilen tarihi yukarıdaki şablona göre parse et
+		t, err := time.Parse(layout, str) //string olarak girilen tarihi yukarıdaki şablona göre parse et
 
-	if err != nil{ //Hata durumda hatayı ekrana yazdır
-		p(err)
+		if err != nil{ //Hata durumda hatayı ekrana yazdır
+			p(err)
+		}
+
+		var day = t.Weekday().String() //parse edilen tarihin haftanın hangi günü olduğunu değişkene ata
+
+		p(m[day])
+
+		p("*********************")
 	}
-
-	p(t.Weekday().String()) //parse edilen tarihin haftanın hangi günü olduğunu ekrana yazdır
 }
